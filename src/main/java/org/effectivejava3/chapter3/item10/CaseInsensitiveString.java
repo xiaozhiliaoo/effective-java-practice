fc9b8ba82c66645ffedd2ba59e1c74efca456334
lili <xiaozhiliaoo@gmail.com>
2020-06-06 16:32:09 +0800

    fix compile

--- src/main/java/org/effectivejava3/chapter3/item10/CaseInsensitiveString.java
+++ src/main/java/org/effectivejava3/chapter3/item10/CaseInsensitiveString.java
@@ -13,10 +13,10 @@ public final class CaseInsensitiveString {
     }
 
     // Broken - violates symmetry!
-    @Override public boolean equals(Object o) {
+    @Override
+    public boolean equals(Object o) {
         if (o instanceof CaseInsensitiveString)
-            return s.equalsIgnoreCase(
-                    ((CaseInsensitiveString) o).s);
+            return s.equalsIgnoreCase(((CaseInsensitiveString) o).s);
         if (o instanceof String)  // One-way interoperability!
             return s.equalsIgnoreCase((String) o);
         return false;
@@ -26,6 +26,9 @@ public final class CaseInsensitiveString {
     public static void main(String[] args) {
         CaseInsensitiveString cis = new CaseInsensitiveString("Polish");
         String s = "polish";
+        System.out.println(cis.equals(s));
+        System.out.println(s.equals(cis));
+
 
         List<CaseInsensitiveString> list = new ArrayList<>();
         list.add(cis);
